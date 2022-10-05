042bbb7a9f5ddd4611fbacdbf0a8e197c3253e07
Elman <elmani3107@gmail.com>
2022-10-05 16:18:47 +0200

    V5

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,76 +1,11 @@
-var nav = $("nav");
-var line = $("<div />").addClass("line");
-
-line.appendTo(nav);
-
-var active = nav.find(".active");
-var pos = 0;
-var wid = 0;
-
-if (active.length) {
-    pos = active.position().left;
-    wid = active.width();
-    line.css({
-        left: pos,
-        width: wid
-    });
-}
-
-nav.find("ul li a").click(function (e) {
-    e.preventDefault();
-    if (!$(this).parent().hasClass("active") && !nav.hasClass("animate")) {
-        nav.addClass("animate");
-
-        var _this = $(this);
-
-        nav.find("ul li").removeClass("active");
-
-        var position = _this.parent().position();
-        var width = _this.parent().width();
-
-        if (position.left >= pos) {
-            line.animate(
-                {
-                    width: position.left - pos + width
-                },
-                300,
-                function () {
-                    line.animate(
-                        {
-                            width: width,
-                            left: position.left
-                        },
-                        150,
-                        function () {
-                            nav.removeClass("animate");
-                        }
-                    );
-                    _this.parent().addClass("active");
-                }
-            );
-        } else {
-            line.animate(
-                {
-                    left: position.left,
-                    width: pos - position.left + wid
-                },
-                300,
-                function () {
-                    line.animate(
-                        {
-                            width: width
-                        },
-                        150,
-                        function () {
-                            nav.removeClass("animate");
-                        }
-                    );
-                    _this.parent().addClass("active");
-                }
-            );
-        }
-
-        pos = position.left;
-        wid = width;
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("menu").style.top = "0";
+    } else {
+        document.getElementById("menu").style.top = "-50px";
     }
-});
+    prevScrollpos = currentScrollPos;
+}
