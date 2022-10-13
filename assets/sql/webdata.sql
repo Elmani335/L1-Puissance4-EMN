ad478a96f99d9b924a40fd77080d528f63a89ec4
MathieuVicic <mathieu.vicic.pro@gmail.com>
2022-10-13 12:16:53 +0200

    oui

--- assets/sql/webdata.sql
+++ assets/sql/webdata.sql
@@ -9,12 +9,12 @@ DROP TABLE IF EXISTS 'utilisateur';
 
 CREATE TABLE IF NOT EXISTS utilisateurs (
 
-    'identifiant' varchar(40) NOT NULL,
-    'email' varchar(60) NOT NULL,
-    'mdp' varchar(40) NOT NULL,
-    'pseudo' varchar(40) NOT NULL,
-    'date-heure-inscription' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    'date-heure-derniere-connexion' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `identifiant` varchar(40) NOT NULL,
+    `email`varchar(60) NOT NULL,
+    `mdp` varchar(40) NOT NULL,
+    `pseudo` varchar(40) NOT NULL,
+    `date-heure-inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `date-heure-derniere-connexion` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ('id')
 
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS 'score';
 
 CREATE TABLE IF NOT EXISTS 'score' (
 
-    'identifiant' varchar(40) NOT NULL,
-    'identifiant-joueur' varchar(40) NOT NULL,
-    'identifiant-jeu' varchar(40) NOT NULL,
-    'difficultÃ©-partie' int(1) NOT NULL AUTO_INCREMENT,
-    'score-partie' int(10) NOT NULL AUTO_INCREMENT,
-    'date-heure-partie' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `identifiant` varchar(40) NOT NULL,
+    `identifiant-joueur` varchar(40) NOT NULL,
+    `identifiant-jeu` varchar(40) NOT NULL,
+    `difficulte-partie` int(1) NOT NULL AUTO_INCREMENT,
+    `score-partie` int(10) NOT NULL AUTO_INCREMENT,
+    `date-heure-partie` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ('id')
 
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
@@ -37,11 +37,11 @@ DROP TABLE IF EXISTS 'message-chat';
 
 CREATE TABLE IF NOT EXISTS 'message-chat' (
 
-    'identifiant' varchar(40) NOT NULL,
-    'identifiant-jeu' varchar(40) NOT NULL,
-    'identifiant-expediteur' varchar(40) NOT NULL,
-    'message' varchar(200) NOT NULL,
-    'date-heure-message' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `identifiant` varchar(40) NOT NULL,
+    `identifiant-jeu` varchar(40) NOT NULL,
+    `identifiant-expediteur` varchar(40) NOT NULL,
+    `message` varchar(200) NOT NULL,
+    `date-heure-message` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ('id')
 
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
@@ -50,8 +50,8 @@ DROP TABLE IF EXISTS 'jeu';
 
 CREATE TABLE IF NOT EXISTS 'jeu' (
 
-    'identifiant' varchar(40) NOT NULL,
-    'nom-jeu' varchar(40) NOT NULL,
+    `identifiant` varchar(40) NOT NULL,
+    `nom-jeu` varchar(40) NOT NULL,
     PRIMARY KEY ('id')
 
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
@@ -62,10 +62,10 @@ DROP TABLE IF EXISTS 'inscription';
 
 CREATE TABLE IF NOT EXISTS 'inscription' (
 
-    'email' varchar(40) NOT NULL,
-    'mdp' varchar(40) NOT NULL,
-    'pseudo' varchar(40) NOT NULL,
-    'date-heure-inscription' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `email` varchar(40) NOT NULL,
+    `mdp` varchar(40) NOT NULL,
+    `pseudo` varchar(40) NOT NULL,
+    `date-heure-inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ('id')
 
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
