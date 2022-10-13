5f40240551375019294201db20fa0fb03090b131
Elman <elmani3107@gmail.com>
2022-10-13 16:27:09 +0200

    update sql
    sql update

--- assets/sql/request.sql
+++ assets/sql/request.sql
@@ -8,20 +8,21 @@ UPDATE utilisateurs SET mdp = '' WHERE id ='' ;
 
 UPDATE utilisateurs SET email = '' WHERE id ='' AND mdp = '';
 
--- Story 4
+-- Story 4 --
 
 SELECT * FROM utilisateurs WHERE email = '' AND mdp = ''
-THEN
-    CONNECT
-ELSE
-    ERROR
+
 
 -- Story 5
 
-INSERT INTO jeu (id, 'nom_jeu')
+INSERT INTO jeu (id, `nom_jeu`)
  VALUES
  ('id', 'Power Of Memory');
 
--- Story 6 
+-- Story 6 --
 
 SELECT 'id_joueur','id_jeu','difficulty','score' FROM score
+
+-- Story 7 --
+
+SELECT 'id_jeu','pseudo','difficulty','score' FROM score AND FROM jeu AND FROM utilisateurs
