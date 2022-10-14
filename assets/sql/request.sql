19e73a6c0b321257204a3657eaa842c9ede8e4a6
Elman <elmani3107@gmail.com>
2022-10-14 10:59:32 +0200

    Update request.sql
    update SQL sto 10

--- assets/sql/request.sql
+++ assets/sql/request.sql
@@ -10,7 +10,7 @@ UPDATE utilisateurs SET email = '' WHERE id ='' AND mdp = '';
 
 -- Story 4 --
 
-SELECT * FROM utilisateurs WHERE email = '' AND mdp = ''
+SELECT * FROM utilisateurs WHERE email = '$' AND mdp = ''
 
 
 -- Story 5
@@ -26,3 +26,15 @@ SELECT 'id_joueur','id_jeu','difficulty','score' FROM score
 -- Story 7 --
 
 SELECT 'id_jeu','pseudo','difficulty','score' FROM score AND FROM jeu AND FROM utilisateurs
+
+-- Story 8 --
+
+UPDATE score SET score = '' WHERE id_joueur = '' AND id_jeu = '' AND difficulty = '';
+
+-- Story 9 --
+
+INSERT INTO message_chat VALUES ('id', 'id_jeu', 'identifiant_expediteur', 'message', 'date_heure_message');
+
+-- Story 10 --
+
+SELECT 'id_jeu','identifiant_expediteur','message','date_heure_message' FROM message_chat AND jeu AND utilisateurs
