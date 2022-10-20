29e685b7ae4d0a4a75d3b19afde1bffb6cd2a9b6
Elman <elmani3107@gmail.com>
2022-10-20 12:06:38 +0200

    Footer css chg
    Update du CSS footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -5,25 +5,25 @@
         <div class="col">
             <h1>Informations</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            <p class="pcol"><strong>Tel :</strong> 0678697969</p>
-            <p class="pcol"><strong>Email :</strong> unEmail@email.com</p>
-            <p class="pcol"><strong>Localisation :</strong> Quelquepart</p>
+            <p class="pcol"><strong>Tel :</strong> 06 01 02 03 04</p>
+            <p class="pcol"><strong>Email :</strong> contact@L1P4.emn</p>
+            <p class="pcol"><strong>Localisation :</strong> Somewhere...</p>
             <a href=""> <i class='bx bxl-facebook-square'></i></a>
             <a href=""> <i class='bx bxl-instagram'></i></a>
             <a href=""> <i class='bx bxl-google'></i></a>
             <a href=""> <i class='bx bxl-pinterest'></i></a>
             <a href=""> <i class='bx bxl-twitter'></i></a>
-            <p class="copy">Copyright @ tout droits réservés</p>
+            <p class="copy">L1 Puissance 4 EMN © All rights reserved - 2022 </p>
         </div>
 
         <div class="col2">
             <h1>Power Of Memory</h1>
             <ul>
                 <li>
-                    <a href="" class="footer_link">Jouer ! </a>
+                    <a href="jeu.php" class="footer_link">Jouer ! </a>
                 </li>
                 <li>
-                    <a href="scores.html" class="footer_link">Les scores</a>
+                    <a href="scores.php" class="footer_link">Les scores</a>
                 </li>
                 <li>
                     <a href="contact.php" class="footer_link">Nous contacter</a>
