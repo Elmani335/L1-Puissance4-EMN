62424ee1550920347fc9ca28546e7f3a9e6f1c54
MathieuVicic <mathieu.vicic.pro@gmail.com>
2022-10-25 11:53:13 +0200

    jeu

--- jeu.php
+++ jeu.php
@@ -1,43 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="assets/css/indexcss/footer.css">
-    <link rel="stylesheet" href="assets/css/indexcss/1.css">
-    <link rel="stylesheet" href="assets/css/indexcss/jeu.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="assets/css/indexcss/header.css">
-    <link rel="stylesheet" href="assets/css/indexcss/contact.css">
-    <meta charset="UTF-8">
-    <title>Jeu</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Memory Game</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/indexcss/jeu.css">
 </head>
+<body class="bodyJeu">
+<?php
+include_once 'assets/includes/header.php';
+?>
 
-    <!-- JEU -->
-    
-    <div class="jeu">
-        <div class="wrapper">
-          <div class="stats-container">
-            <div id="moves-count"></div>
-            <div id="time"></div>
-          </div>
-          <div class="game-container"></div>
-          <button id="stop" class="hide">Stop Game</button>
-        </div>
-        <div class="controls-container">
-          <p id="result"></p>
-          <button id="start">Start Game</button>
-          <a id="button-retour-jeu" href="index.php">Home</a>
-        </div>
-    </div>
-        <!-- Script du jeu -->
-        <script src="assets/js/script.js"></script>
-      
+	<div class="container">
+		<h1>Memory Game</h1>
+		<div class = "dashboard">
+			<div class="dashboard-element">
+				<i class=" dashboard-icon fas fa-stopwatch"></i> <span id="timer"></span>
+			</div>
+			<div class="dashboard-element">
+				<i class=" dashboard-icon fas fa-clone"></i> <span id="moves"></span>
+			</div>
+		</div>
+		<div id="stars">
+			<i class="fas fa-star" id="rating-poor"></i>
+			<i class="fas fa-star" id="rating-average"></i>
+			<i class="fas fa-star" id="rating-perfect"></i>
+		</div>	
+		<div id="form">
+			<input type="radio" id="easyMode"
+			name="difficulty" value="easy">
+			<label for="easyMode">Facile</label>
+			<input type="radio" id="normalMode"
+			name="difficulty" value="normal" checked="checked">
+			<label class="checked" for="normalMode">Intermédiaire</label>
+			<input type="radio" id="hardMode"
+			name="difficulty" value="hard">
+			<label for="hardMode">Difficile</label>
+		</div>
+		<div class="game-board">
+		</div>
+		<i id = "reset" class="fas fa-redo-alt"></i>
+	</div>
+	<div class="modal">
+		<div class="modal-inner">
+			<h3>Bien joué !</h3>
+			<p class="modal-text">Tu as gagné en : <span id="final-time"></span> scondes avec <span id="final-moves"></span> coups</p>
+			<p class="modal-text" id="final-rating"></p>
+			<p class="modal-text" id="replay"> <i class="fas fa-redo-alt"></i> Rejoue ?</p>
+		</div>
+	</div>
+	<script type="text/javascript" src="assets/js/script.js"></script>
 
-
-
-
-
-    
-    
-    </footer>
-
-
+</body>
+</html>
